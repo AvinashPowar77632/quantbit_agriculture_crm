412bd348e4dac09dfc433cdf22704bf01c17bbe7
AvinashPowar77632 <avinashpowar77632@gmail.com>
2026-09-20 09:57:56 +0530

    autoname on trip sheet no

--- quantbit_agriculture_crm/quantbit_agriculture_crm/doctype/trip_sheet/trip_sheet.py
+++ quantbit_agriculture_crm/quantbit_agriculture_crm/doctype/trip_sheet/trip_sheet.py
@@ -7,11 +7,4 @@ from frappe.model.document import Document
 
 class TripSheet(Document):
 	def autoname(self):
-		if not self.season:
-			frappe.throw("Season is required .")
-		if not self.slip_no:
-			frappe.throw("Slip No is required.")
-			
-		season_abbr = frappe.db.get_value("Season", self.season, "abbr")
-		slip_no = str(self.slip_no)
-		self.name = f"TS/{season_abbr}/{slip_no}"
+		self.name = self.trip_sheet
